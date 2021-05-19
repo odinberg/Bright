fd05646ec59b80c6b20a16cf150f68f3d047ddb8
Håkon Korsnes <hkonkorsnes@gmail.com>
2021-05-19 15:36:25 +0200

    no message

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,8 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom"
+//import logo from './images/brightlogo.png';
 
 function App() {
-    return <h1>Hei</h1>;
+    return (
+    <header>
+            {/*<img src={logo} alt="logo"/>*/}
+            <nav>
+                <ul className="nav_links">
+                    <li><a href="#">Service</a></li>
+                    <li><a href="#">Learn</a></li>
+                    <li><a href="#">Scan</a></li>
+                </ul>
+            </nav>
+            <a className="cta" href="#"><button>Help</button></a>
+        </header>
+    )
 }
 
 ReactDOM.render(<App/>, document.getElementById("root"))
