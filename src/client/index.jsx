9a10fb2dc8d11c683159205a2e8b118e1bd36579
Odin <odin.berg@hotmail.co.uk>
2021-05-20 11:18:36 +0200

    Setup for react-router

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,13 +1,36 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import {PageHeader} from "./pageHeader";
+import {Home} from "./home";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {Learn} from "./learn";
+import {Service} from "./service";
+import {Help} from "./help";
+import {Scan} from "./scan";
 
 
 function App() {
     return (
-        <>
+        <Router>
             <PageHeader/>
-        </>
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route exact path="/learn">
+                        <Learn />
+                    </Route>
+                    <Route exact path="/scan">
+                        <Scan />
+                    </Route>
+                    <Route exact path="/service">
+                        <Service />
+                    </Route>
+                    <Route exact path="/help">
+                        <Help/>
+                    </Route>
+                </Switch>
+        </Router>
     )
 }
 
--- src/client/pageHeader.jsx
+++ src/client/pageHeader.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export function PageHeader() {
     return <header>
+        <Link to="/">
         <img className="logo" src="./images/brightlogo.png" alt="logo"/>
+        </Link>
         <nav>
             <ul className="nav_links">
-                <li><a href="#">Service</a></li>
-                <li><a href="#">Learn</a></li>
-                <li><a href="#">Scan</a></li>
+                <li><Link to="/service">Service</Link></li>
+                <li><Link to="/learn">Learn</Link></li>
+                <li><Link to="/scan">Scan</Link></li>
             </ul>
         </nav>
-        <a className="cta" href="#">
+        <Link to="/help">
             <button>Help</button>
-        </a>
+        </Link>
     </header>;
 }
