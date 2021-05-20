69c657805db1b2696cbd8c8ae2bf450b5e483181
Håkon Korsnes <hkonkorsnes@gmail.com>
2021-05-20 15:42:16 +0200

    added all products to servicepage

--- src/client/service.jsx
+++ src/client/service.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 
 import startPlus from "./images/startPlus.png"
+import moveSmart from "./images/moveSmart.png"
+import sunbellSmart from "./images/sunbellSmart.png"
+import sunTurtle from "./images/sunTurtle.png"
 import home from "./images/home.png"
 
 export function Service() {
     return (
-        <div>
-            <img src={startPlus} alt="startplsu" />
-            <img src={home} alt="startplsu" />
+        <div className="procuctContainer">
+            <div className="sunbellSmartDiv">
+                <img className="sunbellSmartImg" src={sunbellSmart} alt="startplus" />
+            </div>
+            <div className="moveSmartDiv">
+                <img className="moveSmartImg" src={moveSmart} alt="startplus" />
+            </div>
+            <div className="startPlusDiv">
+                <img className="startPlusImg" src={startPlus} alt="startplus" />
+            </div>
+            <div className="sunTurtleDiv">
+                <img className="sunTurtleImg" src={sunTurtle} alt="startplus" />
+            </div>
+            <div className="homeDiv">
+                <img className="homeImg" src={home} alt="home" />
+            </div>
         </div>
     );
 }
