ccee03499db783991a458ea82e190278d20f7f94
Odin <odin.berg@hotmail.co.uk>
2021-05-20 16:05:33 +0200

    Added text to products

--- src/client/service.jsx
+++ src/client/service.jsx
@@ -11,18 +11,23 @@ export function Service() {
         <div className="procuctContainer">
             <div className="sunbellSmartDiv">
                 <img className="sunbellSmartImg" src={sunbellSmart} alt="startplus" />
+                <h2 className="sunbellTxt">Sunbell</h2>
             </div>
             <div className="moveSmartDiv">
                 <img className="moveSmartImg" src={moveSmart} alt="startplus" />
+                <h2 className="moveSmartTxt">Move smart</h2>
             </div>
             <div className="startPlusDiv">
                 <img className="startPlusImg" src={startPlus} alt="startplus" />
+                <h2 className="startPlusTxt">Start plus</h2>
             </div>
             <div className="sunTurtleDiv">
                 <img className="sunTurtleImg" src={sunTurtle} alt="startplus" />
+                <h2 className="sunTurtleTxt">SunTurtle</h2>
             </div>
             <div className="homeDiv">
                 <img className="homeImg" src={home} alt="home" />
+                <h2 className="homeTxt">Home</h2>
             </div>
         </div>
     );
