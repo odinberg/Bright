9fa2b0b6462d00e772e6030704d1f47e85b23988
aliankf <aliankf1106@gmail.com>
2021-05-25 22:44:08 +0200

    changed size and fixed text

--- src/client/learn.jsx
+++ src/client/learn.jsx
@@ -5,7 +5,7 @@ import YoutubeEmbed from "./youtubeEmbed";
 export function Learn() {
     return (
         <div className="video-container">
-            <h1>Learningvideo</h1>
+            <h1>Learningvideo:</h1>
             <YoutubeEmbed embedId="uJL3-Vwplio" />
         </div>
         );
