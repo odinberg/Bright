6cd626752a53ba13bd6bdbd7b59dcc6ebb6f4069
Håkon Korsnes <hkonkorsnes@gmail.com>
2021-05-25 21:47:11 +0200

    commented out some more of the logo image. cleaned up code a little

--- src/client/pageHeader.jsx
+++ src/client/pageHeader.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import logo from "./images/brightlogo.png"
+{/*import logo from "./images/brightlogo.png"*/}
 
 
 export function PageHeader(){
