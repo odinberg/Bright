7c7fdac70fbe56de2ac22b4721bf9fd2b813b72a
Håkon Korsnes <hkonkorsnes@gmail.com>
2021-05-30 21:03:16 +0200

    added searchbar

--- src/client/pageHeader.jsx
+++ src/client/pageHeader.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import logo from "./images/brightLogo.png"
+import {SearchBar} from "./searchBar"
 
 
 export function PageHeader(){
@@ -16,6 +17,7 @@ export function PageHeader(){
                 <li><Link to="/learn">Learn</Link></li> 
             </ul>
         </nav>
+        <SearchBar></SearchBar>
         <Link to="/help">
             <button>Help</button>
         </Link>
