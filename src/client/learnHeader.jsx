87465aa3e1b37bf3c30ca05b6c9c4c23686b001e
Håkon Korsnes <hkonkorsnes@gmail.com>
2021-06-01 17:59:27 +0200

    rearranged video links, and centered it

--- src/client/learnHeader.jsx
+++ src/client/learnHeader.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import { Products } from "./Products";
 
 
 export function LearnHeader(){
@@ -8,10 +9,11 @@ export function LearnHeader(){
     <header className="learn-header">
         <nav>
             <ul className="video_links">
-                <li><Link to="/startVideo">How-to Start+</Link></li>
-                <li><Link to="/moveVideo">How-to Move</Link></li>
-                <li><Link to="/sunbellVideo">How-to Sunbell</Link></li>
+                <li><Link to="/sunbellVideo">How to Sunbell Smart</Link></li>
+                <li><Link to="/moveVideo">How to Move Smart</Link></li>
+                <li><Link to="/startVideo">How to Start+</Link></li>
             </ul>
         </nav>
     </header>
 )}
+
