12de48716a342f58f9671ac10dda33ace415ba3d
Håkon Korsnes <hkonkorsnes@gmail.com>
2021-06-01 23:30:03 +0200

    changed some stuff on learn page to style it, but its bugging out

--- src/client/learnHeader.jsx
+++ src/client/learnHeader.jsx
@@ -8,29 +8,28 @@ import sunbellSmart from "./images/products/sunbellSmart.png";
 export function LearnHeader(){
 
     return(
-    <header className="learn-header">
-        <nav>
-            <div className="video_links">
-                <Link to="/sunbellVideo">
-                    <div className="sunbellLink">
-                        <img className="sunbellSmartImg" src={sunbellSmart}></img>
-                        <li>How to Sunbell Smart</li>
-                    </div>
-                </Link>
-                <Link to="/moveVideo">
-                    <div className="moveLink">
-                        <img className="moveSmartImg"src={moveSmart}></img>
-                        <li>How to Move Smart</li>
-                    </div>
-                </Link>
-                <Link to="/startVideo">
-                    <div className="startPlusLink">
-                        <img className="startPlusImg" src={startPlus}></img>
-                        <li>How to Start+</li>
-                    </div>
-                </Link>
-            </div>
-        </nav>
-    </header>
+    <div className="learn-header">
+        
+        <div className="video_links">
+            <Link to="/sunbellVideo">
+                <div className="sunbellLink">
+                    <img className="sunbellSmartImg" src={sunbellSmart}></img>
+                    <p>How to Sunbell Smart</p>
+                </div>
+            </Link>
+            <Link to="/moveVideo">
+                <div className="moveLink">
+                    <img className="moveSmartImg"src={moveSmart}></img>
+                    <p>How to Move Smart</p>
+                </div>
+            </Link>
+            <Link to="/startVideo">
+                <div className="startPlusLink">
+                    <img className="startPlusImg" src={startPlus}></img>
+                    <p>How to Start+</p>
+                </div>
+            </Link>
+        </div>
+    </div>
 )}
 
