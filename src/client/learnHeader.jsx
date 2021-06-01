65deade5228bb761e61cb9b5f7e90fb499030fc6
aliankf <aliankf1106@gmail.com>
2021-06-02 00:22:06 +0200

    Fixed images so they appear side by side in center and added text under

--- src/client/learnHeader.jsx
+++ src/client/learnHeader.jsx
@@ -8,25 +8,25 @@ import sunbellSmart from "./images/products/sunbellSmart.png";
 export function LearnHeader(){
 
     return(
-    <div className="learn-header">
+    <div className="learnDiv">
         
         <div className="video_links">
             <Link to="/sunbellVideo">
                 <div className="sunbellLink">
-                    <img className="sunbellSmartImg" src={sunbellSmart}></img>
-                    <p>How to Sunbell Smart</p>
+                    <img className="sunbellSmartLearnImg" src={sunbellSmart}></img>
+                    <p className="learnSunbellTxt">How to Sunbell Smart</p>
                 </div>
             </Link>
             <Link to="/moveVideo">
                 <div className="moveLink">
                     <img className="moveSmartImg"src={moveSmart}></img>
-                    <p>How to Move Smart</p>
+                    <p className="learnMoveTxt">How to Move Smart</p>
                 </div>
             </Link>
             <Link to="/startVideo">
                 <div className="startPlusLink">
                     <img className="startPlusImg" src={startPlus}></img>
-                    <p>How to Start+</p>
+                    <p className="learnSmartTxt">How to Start+</p>
                 </div>
             </Link>
         </div>
